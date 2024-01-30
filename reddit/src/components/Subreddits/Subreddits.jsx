a968979fef115010de82d822e38f4bf980acd4a6
04stefke <jovanovics399@yahoo.com>
2024-01-30 17:58:48 +0000

    managed to get the subreddit data and displayed them correctly

--- reddit/src/components/Subreddits/Subreddits.jsx
+++ reddit/src/components/Subreddits/Subreddits.jsx
@@ -1,29 +1,32 @@
 import { Box, Button, Stack } from '@mui/material'
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import {getSubredditsData} from '../API/Api'
 const Subreddits = () => {
-    
+    const dispatch = useDispatch()
+    const subredditsData = useSelector((state) => state.subreddits.subreddits.data?.children)
 
-    const subreddit = (
-        <Box sx={{padding: '30px', paddingBottom: '10px', flexDirection: {sm: 'column', md: 'row'}}}>
-            <Button sx={{bgcolor:'red', color:'white'}} >Subreddits</Button>
-        </Box>
+    useEffect(() => {
+        dispatch(getSubredditsData())
+    }, [dispatch])
+
+    const subreddit = subredditsData && subredditsData.length > 0 ? (
+        subredditsData.map(item => (
+            <Box key={item.data.id} sx={{padding: '30px', paddingBottom: '0', flexDirection: {sm: 'column', md: 'row'}}}>
+                <Button sx={{bgcolor:'red', color:'white'}} >{item.data.display_name}</Button>
+            </Box> 
+        ))
+    ) : (
+        <Box>No subreddits</Box>
     )
+
+    // const subreddit = (
+    //    
+    // )
 
     return (
         <Stack sx={{borderRight: '1px solid #3d3d3d', width: 'fit-content'}}>
             {subreddit}
-            {subreddit}
-            {subreddit}
-            {subreddit}
-            {subreddit}
-            {subreddit}
-            {subreddit}
-            {subreddit}
-            {subreddit}
-            {subreddit}
-            {subreddit}
-
         </Stack>
     )
 }
--- reddit/src/components/Subreddits/SubredditsSlice.jsx
+++ reddit/src/components/Subreddits/SubredditsSlice.jsx
@@ -1,7 +1,7 @@
 import {createSlice} from '@reduxjs/toolkit'
 
 export const SubredditsSlice = createSlice({
-    name: 'subreddit',
+    name: 'subreddits',
     initialState: {
         subreddits: []
     }, 
