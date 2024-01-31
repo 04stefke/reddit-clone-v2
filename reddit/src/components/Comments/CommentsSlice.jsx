7ff8ca799161b564109e7b7a1e18da8e458e448b
04stefke <jovanovics399@yahoo.com>
2024-01-31 10:27:03 +0000

    created the comments slice

---
+++ reddit/src/components/Comments/CommentsSlice.jsx
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const CommentsSLice = createSlice({
+    name: 'comments',
+    initialState: {
+        comments: [],
+        selectedComments: '',
+        selectedTitle: '',
+        showButton: 'Show Comments'
+    },
+    reducers:{
+        setComments(state, action){
+            state.comments = action.payload
+        },
+        setSelectedComment(state, action){
+            state.selectedComments = action.payload
+        },
+        setSelectedTitle(state, action){
+            state.selectedTitle = action.payload
+        },
+        setButton(action, state){
+            state.showButton = action.payload
+        }
+    }
+})
+
+export const {
+    setComments,
+    setSelectedComment,
+    setSelectedTitle,
+    setButton
+} = CommentsSLice.actions
+
+export default CommentsSLice.reducer
